bd3a36b1a03407800e0116e21e9017f73e51d422
ykaraman <yusufokaraman@gmail.com>
2025-03-23 23:48:30 +0300

    Stack node class added.

---
+++ Stack/Node.cs
@@ -0,0 +1,15 @@
+﻿namespace Stack
+{
+    class Node
+    {
+        public int data { get; set; }
+
+        public Node next { get; set; }
+
+        public Node(int data)
+        {
+           this.data = data;
+            next = null;
+        }
+    }
+}
