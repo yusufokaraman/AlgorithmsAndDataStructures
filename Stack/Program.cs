8c7e74996295c75973b7dd1c7211e7ccd278fde8
ykaraman <yusufokaraman@gmail.com>
2025-03-24 00:00:35 +0300

    Stack Push() PrintAll() methods added.

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.PrintAll();
         }
     }
 }
---
+++ Stack/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.Design;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private Node top;
+        private int count;
+
+        public Stack()
+        {
+            top = null;
+            count = 0;
+        }
+
+        public Stack(int data)
+        {
+            var newNode = new Node(data);
+            top = newNode;
+            count = 1;
+        }
+
+        public void Push(int data)
+        {
+            var newNode = new Node(data);
+            if (count == 0)
+                top = newNode;
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+            count++;
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine("Stack içeriği:");
+            Console.WriteLine("- - - - - - - -");
+            Node tmp = top;
+
+            while (tmp != null)
+            {
+                Console.WriteLine(tmp.data);
+                tmp = tmp.next;
+            }
+        }
+
+    }
+}
